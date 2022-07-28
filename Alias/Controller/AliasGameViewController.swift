06500ecce5b1f89cd4bbca969f7d21ff26cd7fc7
terentyev-va <terentyev.va@yandex.ru>
2022-07-28 16:56:03 +0300

    Create AliasGameViewController and ResultsViewController

---
+++ Alias/Controller/AliasGameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AliasGameViewController.swift
+//  Alias
+//
+//  Created by Вячеслав Терентьев on 28.07.2022.
+//
+
+import UIKit
+
+class AliasGameViewController: UIViewController {
+    
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var wordsLabel: UILabel!
+    
+    var aliasGameManager: AliasGameManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func correctButtonPressed() {
+        
+    }
+    
+    @IBAction func skipButtonPressed() {
+        
+    }
+    
+    @IBAction func resetButtonPressed() {
+        
+    }
+}
---
+++ Alias/Controller/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  Alias
+//
+//  Created by Вячеслав Терентьев on 28.07.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var jokeSetupLabel: UILabel!
+    @IBOutlet var jokePunchlineLabel: UILabel!
+    
+    var aliasGameManager: AliasGameManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func resetButtonPressed() {
+        
+    }
+}
