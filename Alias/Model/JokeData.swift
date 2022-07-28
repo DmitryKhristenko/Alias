aee90c20a495e69a20712bc48d5908f3f321b477
terentyev-va <terentyev.va@yandex.ru>
2022-07-28 20:27:54 +0300

    Create JokeModel and parsing JSON

---
+++ Alias/Model/JokeData.swift
@@ -0,0 +1,13 @@
+//
+//  JokeData.swift
+//  Alias
+//
+//  Created by Вячеслав Терентьев on 28.07.2022.
+//
+
+import Foundation
+
+struct JokeData: Decodable {
+    let setup: String
+    let punchline: String
+}
---
+++ Alias/Model/JokeManager.swift
@@ -0,0 +1,47 @@
+//
+//  JokeManager.swift
+//  Alias
+//
+//  Created by Вячеслав Терентьев on 28.07.2022.
+//
+
+import Foundation
+
+struct JokeManager {
+    
+    let jokeURL = "https://joke.deno.dev"
+    
+    func fetchJoke() {
+        let urlString = jokeURL
+        print(urlString)
+    }
+    
+    func getJoke() {
+        
+        if let url = URL(string: jokeURL) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(jokeData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(jokeData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(JokeData.self, from: jokeData)
+            let jokeSetup = decodedData.setup
+            let punchLine = decodedData.punchline
+            print(jokeSetup, punchLine)
+        } catch {
+            print(error)
+        }
+    }
+}
