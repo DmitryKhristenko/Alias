0a39af679e11404c84ed3402c4cc1b16ac20a58b
AndreyBulganin <bulganinworkbox@gmail.com>
2022-07-29 21:27:44 +0300

    - added the timer

--- Alias/Controller/AliasGameViewController.swift
+++ Alias/Controller/AliasGameViewController.swift
@@ -12,16 +12,24 @@ class AliasGameViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var wordsLabel: UILabel!
-        
+    @IBOutlet var correctButton: UIButton!
+    
+    
     var aliasGameManager: AliasGameManager!
+    private var timer = Timer()
+    private var totalTime = 60
+    private var secondsPassed = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        updateUI()
+        correctButton.setTitle("Начать игру", for: .normal)
     }
     
     @IBAction func correctButtonPressed() {
+        if correctButton.titleLabel?.text == "Начать игру" {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        }
+        correctButton.setTitle("Правильно", for: .normal)
         aliasGameManager.scoreUp()
         updateUI()
     }
@@ -40,4 +48,15 @@ class AliasGameViewController: UIViewController {
         scoreLabel.text = String(aliasGameManager.score)
         wordsLabel.text = aliasGameManager.getWord()
     }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            timerLabel.text = String(totalTime - secondsPassed)
+        } else {
+            timer.invalidate()
+            performSegue(withIdentifier: "showResults", sender: nil)
+        }
+    }
+    
 }
