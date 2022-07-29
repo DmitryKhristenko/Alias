099c3e9539cb7ad68e85a60fecd623731765b7c1
terentyev-va <terentyev.va@yandex.ru>
2022-07-29 08:33:59 +0300

    Fixed JokeManager

--- Alias/Model/JokeManager.swift
+++ Alias/Model/JokeManager.swift
@@ -2,36 +2,28 @@
 //  JokeManager.swift
 //  Alias
 //
-//  Created by Вячеслав Терентьев on 28.07.2022.
+//  Created by Вячеслав Терентьев on 29.07.2022.
 //
 
 import Foundation
 
-protocol JokeManagerDelegate {
-    
-    func didUpdateJoke(_ jokeSetup: JokeManager, jokePunchline: JokeData)
-    func didFailWithError(error: Error)
-}
-
 struct JokeManager {
     
-    private let jokeURL = "https://joke.deno.dev"
+    private let jokeURL = "https://joke.deno.dev/"
     
-    var delegate: JokeManagerDelegate?
-        
-    func getJoke() {
+    func fetchJoke(completion: @escaping (JokeData) -> Void) {
         
         if let url = URL(string: jokeURL) {
             let session = URLSession(configuration: .default)
+            
             let task = session.dataTask(with: url) { data, response, error in
                 if error != nil {
-                    delegate?.didFailWithError(error: error!)
                     return
                 }
+                
                 if let safeData = data {
                     if let joke = parseJSON(safeData) {
-                        delegate?.didUpdateJoke(self, jokePunchline: joke)
-
+                        completion(joke)
                     }
                 }
             }
@@ -43,13 +35,10 @@ struct JokeManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(JokeData.self, from: jokeData)
-            let jokeSetup = decodedData.setup
-            let punchLine = decodedData.punchline
+            let joke = decodedData
             
-            let joke = JokeData(setup: jokeSetup, punchline: punchLine)
             return joke
         } catch {
-            delegate?.didFailWithError(error: error)
             return nil
         }
     }
