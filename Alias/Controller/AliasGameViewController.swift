15f48d04b3fd77ddf667e446b1cab48f2ba799c0
terentyev-va <terentyev.va@yandex.ru>
2022-07-29 11:43:37 +0300

    Button Implementation(Correct, Skip, Reset)

--- Alias/Controller/AliasGameViewController.swift
+++ Alias/Controller/AliasGameViewController.swift
@@ -13,6 +13,8 @@ class AliasGameViewController: UIViewController {
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var wordsLabel: UILabel!
     
+    var userScore = 0
+    
     var aliasGameManager: AliasGameManager!
     
     override func viewDidLoad() {
@@ -20,14 +22,21 @@ class AliasGameViewController: UIViewController {
     }
     
     @IBAction func correctButtonPressed() {
-        
+        userScore += 1
+        scoreLabel.text = "\(userScore)"
     }
     
     @IBAction func skipButtonPressed() {
-        
+        if userScore == 0 {
+            userScore = 0
+        } else {
+            userScore -= 1
+            scoreLabel.text = "\(userScore)"
+        }
     }
     
     @IBAction func resetButtonPressed() {
-        
+        scoreLabel.text = "\(0)"
+        userScore = 0
     }
 }
